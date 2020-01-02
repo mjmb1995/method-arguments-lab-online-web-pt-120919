152a437bc702c24563cca483e71b9e6cc5215126
Mark Bihag <markbihag330@gmail.com>
2020-01-02 06:46:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def introcution_with_language(name, language)
+  puts "Hi, my name is #{} and I am learinng to program in #{language}."
